1cc03b62154ce5e2d29d1d6ca098fa06c8c55e2f
Bombataeter4 <gebakkenam@gmail.com>
2025-08-27 21:05:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Vervang dit zo met jouw Vercel-URL (stap 4):
-const PROXY_URL = "jessepinkmanbot-rj0doxzjb-atiebombaties-projects.vercel.app";
+const PROXY_URL = "jessepinkmanbot-b70oh7o58-atiebombaties-projects.vercel.app";
 
 const chatEl = document.getElementById("chat");
 const form = document.getElementById("form");
